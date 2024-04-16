9c5c64b64ae9fab10d35a4d23f69f48a21ad6c86
Victor Ochoa <victor.silva@luizalabs.com>
2024-04-16 17:11:41 -0300

    fix: remove project

--- src/Domain/OMG.Domain.Core/Base/ValueObject.cs
+++ src/Domain/OMG.Domain.Core/Base/ValueObject.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OMG.Domain.Core.Base;
+﻿namespace OMG.Domain.Core.Base;
 public abstract class ValueObject
 {
     protected static bool EqualOperator(ValueObject left, ValueObject right)
