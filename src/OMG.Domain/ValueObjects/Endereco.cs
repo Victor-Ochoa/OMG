813339c16f6b3b34008c0777212624ca8079d419
Victor-Ochoa <victor20054@gmail.com>
2024-04-17 09:38:00 -0300

    format endereco document

--- src/OMG.Domain/ValueObjects/Endereco.cs
+++ src/OMG.Domain/ValueObjects/Endereco.cs
@@ -1,32 +1,16 @@
 ﻿using OMG.Domain.Base;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OMG.Domain.ValueObjects;
 
-public class Endereco : ValueObject
+public class Endereco(string rua, string numero, string bairro, string cidade, string estado, string cep, string complemento = "") : ValueObject
 {
-    public Endereco(string rua, string numero, string bairro, string cidade, string estado, string cep, string complemento = "")
-    {
-        Rua = rua;
-        Numero = numero;
-        Bairro = bairro;
-        Complemento = complemento;
-        Cidade = cidade;
-        Estado = estado;
-        CEP = cep;
-    }
-
-    public string Rua { get; }
-    public string Numero { get; }
-    public string Bairro { get; set; }
-    public string Complemento { get; }
-    public string Cidade { get; }
-    public string Estado { get; }
-    public string CEP { get; }
+    public string Rua { get; } = rua;
+    public string Numero { get; } = numero;
+    public string Bairro { get; set; } = bairro;
+    public string Complemento { get; } = complemento;
+    public string Cidade { get; } = cidade;
+    public string Estado { get; } = estado;
+    public string CEP { get; } = cep;
 
     protected override IEnumerable<object> GetEqualityComponents()
     {
@@ -39,9 +23,6 @@ public class Endereco : ValueObject
         yield return CEP;
     }
 
-    public override string ToString()
-    {
-        return $"{Rua}, nº {Numero}, {Bairro}, {Cidade}, {Estado}, CEP: {CEP} ";
-    }
+    public override string ToString() => $"{Rua}, nº {Numero}, {Bairro}, {Cidade}, {Estado}, CEP: {CEP} ";
 }
 
